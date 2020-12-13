5de6ab100e7ce12956efbaf1f791771893462e75
Pronesti <dieh.diego@gmail.com>
2020-12-13 11:03:54 -0300

    Improve user factories

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -25,3 +25,55 @@ $factory->define(User::class, function (Faker $faker) {
         'remember_token' => Str::random(10),
     ];
 });
+
+$factory->state(\App\User::class, 'my_user',
+    [
+        'name' => 'Diego Pronesti',
+        'username' => 'diegodieh',
+        'email' => 'dieh.diego@gmail.com',
+        'email_verified_at' => now(),
+        'password' => '$2y$10$YSoEYBiirKDcftwqqMIYDOTueCXFk4TvJg9FS37Hc8i.sDsCfn44m', // password
+        'remember_token' => Str::random(10),
+    ]);
+    
+
+$factory->state(\App\User::class, 'with_profile',[])
+        ->afterCreatingState(\App\User::class, 'with_profile', function ($user,Faker $faker){
+            $user->profile->update(factory(\App\Profile::class)->raw());
+        });
+
+$factory->state(\App\User::class, 'with_posts',[])
+        ->afterCreatingState(\App\User::class, 'with_posts', function ($user,Faker $faker){
+            factory(\App\Post::class, 5)->create(['user_id' => $user->id]);
+        });
+
+$factory->state(\App\User::class, 'with_stories',[])
+        ->afterCreatingState(\App\User::class, 'with_stories', function ($user,Faker $faker){
+            factory(\App\Story::class, 5)->create(['user_id' => $user->id]);
+});
+
+$factory->state(\App\User::class, 'with_follow',[])
+        ->afterCreatingState(\App\User::class, 'with_follows', function ($user,Faker $faker){
+            $FOLLOWS_PER_USER = 10;
+            for($i=0;$i < $FOLLOWS_PER_USER;$i++){
+                 $user->following()->toggle(\App\Profile::find($faker->numberBetween(0,\App\User::count())));
+             }
+});
+
+$factory->state(\App\User::class, 'with_likes',[])
+        ->afterCreatingState(\App\User::class, 'with_likes', function ($user,Faker $faker){
+            $LIKES_PER_USER = 10;
+            for($i=0;$i < $LIKES_PER_USER;$i++){
+                 $post = \App\Post::inRandomOrder()->whereIn('user_id',$user->following->pluck('id'))->limit(1)->get();
+                 $user->likedPosts()->toggle($post);
+             }
+});
+
+$factory->state(\App\User::class, 'with_saves',[])
+        ->afterCreatingState(\App\User::class, 'with_saves', function ($user,Faker $faker){
+            $SAVES_PER_USER = 10;
+            for($i=0;$i < $SAVES_PER_USER;$i++){
+                 $post = \App\Post::inRandomOrder()->whereIn('user_id',$user->following->pluck('id'))->limit(1)->get();
+                 $user->saves()->toggle($post);
+             }
+});
--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,25 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(MyUserSeeder::class);
-        $USERS_NUMBER = 10;
-        $POSTS_PER_USER = 5;
-        $STORIES_PER_USER = 5;
-        $FOLLOWS_PER_USER = 10;
-
-        $faker = Faker\Factory::create();
-        $faker->addProvider(new \Faker\Provider\en_US\Address($faker));
-        factory(App\User::class, $USERS_NUMBER)->create()->each(function ($user) use($faker, $POSTS_PER_USER, $USERS_NUMBER,$FOLLOWS_PER_USER,$STORIES_PER_USER){
-            $user->profile->update(factory(\App\Profile::class)->raw());
-            $user->posts()->saveMany(
-                factory(App\Post::class,$POSTS_PER_USER)->make(['user_id' => $user->id])
-            );
-            $user->stories()->saveMany(
-                factory(App\Story::class,$STORIES_PER_USER)->make(['user_id' => $user->id])
-            );
-            for($i=0;$i < $FOLLOWS_PER_USER;$i++){
-                $user->following()->toggle(\App\Profile::find($faker->numberBetween(0,$USERS_NUMBER)));
-            }
-        });
+        //$this->call(MyUserSeeder::class);
+        DB::beginTransaction();
+        factory('App\User',10)->states('with_profile','with_posts','with_stories','with_follows','with_likes','with_saves')->create();
+        factory('App\User',1)->states('my_user','with_profile','with_posts','with_stories','with_follows','with_likes','with_saves')->create();
+        DB::commit();
     }
 }
